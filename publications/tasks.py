045d6490763fef101759be8efdbb6f30467bc428
FroVolod <frol_off@meta.ua>
2020-02-13 21:40:06 +0200

    Finished

--- publications/tasks.py
+++ publications/tasks.py
@@ -1,17 +1,15 @@
 from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
 
-from mynews.settings import DEFAULT_FROM_EMAIL
 from mynews.celery import app
  
  
 @app.task
-def send_email_to_author(email, post):
+def send_email_to_author(receipient, post):
     subject = "Hello, its MYNEWS"
     text_content = f'You have received a new message in your publication: {post}'
     html_content = f'<p>You have received a new message in your publication:</p> \
                     <p><strong>{post}</strong></p>'
-    sender = DEFAULT_FROM_EMAIL
-    receipient = email
-    msg = EmailMultiAlternatives(subject, text_content, sender, [receipient])
+    msg = EmailMultiAlternatives(subject, text_content, settings.DEFAULT_FROM_EMAIL, [receipient])
     msg.attach_alternative(html_content, "text/html")
     respone = msg.send()
